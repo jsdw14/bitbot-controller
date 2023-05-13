8792fb23e04912ea7646f73f565ae709807dce60
jsdw14 <jsdw@icloud.com>
2023-05-13 16:44:29 +1000

    added signal strength indicator

--- main.ts
+++ main.ts
@@ -15,19 +15,33 @@ input.onPinPressed(TouchPin.P1, function () {
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "drive") {
+        bitbot.ledRainbow()
         if (value == 1) {
-            bitbot.rotate(BBRobotDirection.Left, 60)
+            bitbot.rotate(BBRobotDirection.Left, 40)
+            bitbot.ledRotate()
         } else if (value == 2) {
-            bitbot.rotate(BBRobotDirection.Right, 60)
+            bitbot.rotate(BBRobotDirection.Right, 40)
+            bitbot.ledRotate()
         } else if (value == 0) {
             bitbot.go(BBDirection.Forward, 60)
+            bitbot.ledRotate()
         } else if (value == 10) {
             bitbot.go(BBDirection.Reverse, 60)
+            bitbot.setLedColor(0x00FF00)
         } else if (value == 11) {
             bitbot.stop(BBStopMode.Brake)
+            bitbot.setLedColor(0xFF0000)
         }
     }
 })
+let signal_val = 0
 basic.showIcon(IconNames.Giraffe)
 radio.setGroup(1)
 bitbot.select_model(BBModel.XL)
+basic.forever(function () {
+    signal_val = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    led.plotBarGraph(
+    signal_val,
+    50
+    )
+})
