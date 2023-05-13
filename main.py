fdee149bba6c48a24d78c6867a2f4fd003b47819
jsdw14 <jsdw@icloud.com>
2023-05-13 19:25:32 +1000

    simplified version which seems to be more reliable

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
+def on_pulsed_p16_high():
+    basic.show_icon(IconNames.STICK_FIGURE)
+    radio.send_value("drive", 1)
+pins.on_pulsed(DigitalPin.P16, PulseValue.HIGH, on_pulsed_p16_high)
+
 def on_pin_pressed_p0():
     radio.send_value("drive", 0)
 input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
 
-def on_button_pressed_a():
-    radio.send_value("drive", 10)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+def my_function():
+    radio.send_value("drive", 16)
+Kitronik_Game_Controller.on_button_press(Kitronik_Game_Controller.ControllerButtonPins.FIRE2,
+    Kitronik_Game_Controller.ControllerButtonEvents.DOWN,
+    my_function)
+
+def my_function2():
+    radio.send_value("lights", 16)
+Kitronik_Game_Controller.on_button_press(Kitronik_Game_Controller.ControllerButtonPins.FIRE2,
+    Kitronik_Game_Controller.ControllerButtonEvents.UP,
+    my_function2)
 
 def on_pin_pressed_p2():
     radio.send_value("drive", 2)
 input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
 
 def on_button_pressed_b():
-    radio.send_value("drive", 11)
+    radio.send_value("drive", 16)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_pin_pressed_p1():
     radio.send_value("drive", 1)
 input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
 
+def on_pulsed_p8_high():
+    basic.show_icon(IconNames.STICK_FIGURE)
+    radio.send_value("drive", 8)
+pins.on_pulsed(DigitalPin.P8, PulseValue.HIGH, on_pulsed_p8_high)
+
 def on_received_value(name, value):
     if name == "drive":
-        bitbot.led_rainbow()
         if value == 1:
             bitbot.rotate(BBRobotDirection.LEFT, 40)
             bitbot.led_rotate()
@@ -30,14 +47,36 @@ def on_received_value(name, value):
         elif value == 0:
             bitbot.go(BBDirection.FORWARD, 60)
             bitbot.led_rotate()
-        elif value == 10:
+        elif value == 8:
             bitbot.go(BBDirection.REVERSE, 60)
             bitbot.set_led_color(0x00FF00)
-        elif value == 11:
+        elif value == 16:
             bitbot.stop(BBStopMode.BRAKE)
             bitbot.set_led_color(0xFF0000)
+    elif name == "lights":
+        if value == 12:
+            music.play_sound_effect(music.builtin_sound_effect(soundExpression.giggle),
+                SoundExpressionPlayMode.IN_BACKGROUND)
+            bitbot.led_rainbow()
+        elif value == 16:
+            bitbot.led_rainbow()
 radio.on_received_value(on_received_value)
+
+def my_function3():
+    radio.send_value("lights", 12)
+Kitronik_Game_Controller.on_button_press(Kitronik_Game_Controller.ControllerButtonPins.LEFT,
+    Kitronik_Game_Controller.ControllerButtonEvents.CLICK,
+    my_function3)
+
+def my_function4():
+    radio.send_value("drive", 8)
+Kitronik_Game_Controller.on_button_press(Kitronik_Game_Controller.ControllerButtonPins.FIRE1,
+    Kitronik_Game_Controller.ControllerButtonEvents.CLICK,
+    my_function4)
 
 basic.show_icon(IconNames.GIRAFFE)
 radio.set_group(1)
 bitbot.select_model(BBModel.XL)
+bitbot.led_clear()
+pins.set_events(DigitalPin.P8, PinEventType.TOUCH)
+pins.set_events(DigitalPin.P16, PinEventType.TOUCH)
--- main.ts
+++ main.ts
@@ -1,46 +1,40 @@
 input.onPinPressed(TouchPin.P0, function () {
     radio.sendValue("drive", 0)
 })
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Click, function () {
-    radio.sendValue("drive", 10)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire2, Kitronik_Game_Controller.ControllerButtonEvents.Click, function () {
-    radio.sendValue("drive", 11)
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("drive", 8)
 })
 input.onPinPressed(TouchPin.P2, function () {
     radio.sendValue("drive", 2)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("drive", 16)
 })
 input.onPinPressed(TouchPin.P1, function () {
     radio.sendValue("drive", 1)
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "drive") {
-        bitbot.ledRainbow()
         if (value == 1) {
             bitbot.rotate(BBRobotDirection.Left, 40)
-            bitbot.ledRotate()
+            bitbot.ledRainbow()
         } else if (value == 2) {
             bitbot.rotate(BBRobotDirection.Right, 40)
-            bitbot.ledRotate()
+            bitbot.ledRainbow()
         } else if (value == 0) {
             bitbot.go(BBDirection.Forward, 60)
-            bitbot.ledRotate()
-        } else if (value == 10) {
+            bitbot.ledRainbow()
+        } else if (value == 8) {
             bitbot.go(BBDirection.Reverse, 60)
-            bitbot.setLedColor(0x00FF00)
-        } else if (value == 11) {
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
+            bitbot.ledRainbow()
+        } else if (value == 16) {
             bitbot.stop(BBStopMode.Brake)
             bitbot.setLedColor(0xFF0000)
         }
-    } else if (name == "lights") {
-        if (value == 0) {
-            music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
-        }
     }
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Click, function () {
-    radio.sendValue("lights", 0)
 })
 basic.showIcon(IconNames.Giraffe)
 radio.setGroup(1)
 bitbot.select_model(BBModel.XL)
+bitbot.ledClear()
