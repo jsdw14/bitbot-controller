baf7a4d8cf3df432b255014f74325a7022c9c9fd
jsdw14 <jsdw@icloud.com>
2023-05-13 16:18:35 +1000

    fixing reverse

--- main.ts
+++ main.ts
@@ -2,14 +2,15 @@ input.onPinPressed(TouchPin.P0, function () {
     radio.sendValue("drive", 0)
 })
 input.onButtonPressed(Button.A, function () {
+    radio.sendValue("drive", 10)
     bitbot.move(BBMotor.Both, BBDirection.Reverse, 60)
 })
 input.onPinPressed(TouchPin.P2, function () {
     radio.sendValue("drive", 2)
 })
 input.onButtonPressed(Button.B, function () {
-    bitbot.stop(BBStopMode.Brake)
     radio.sendValue("drive", 10)
+    bitbot.stop(BBStopMode.Brake)
 })
 input.onPinPressed(TouchPin.P1, function () {
     radio.sendValue("drive", 1)
@@ -22,6 +23,8 @@ radio.onReceivedValue(function (name, value) {
             bitbot.move(BBMotor.Left, BBDirection.Forward, 60)
         } else if (value == 0) {
             bitbot.move(BBMotor.Both, BBDirection.Forward, 60)
+        } else if (value == 10) {
+            bitbot.move(BBMotor.Both, BBDirection.Reverse, 60)
         }
     }
 })
