3c5697469ff5886a3fa925d913ab12033d30ee27
jsdw14 <jsdw@icloud.com>
2023-05-18 18:13:22 +1000

    added variable speed

--- main.ts
+++ main.ts
@@ -7,6 +7,14 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUT
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     radio.sendValue("drive", 0)
 })
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    if (logo_press == 0) {
+        basic.showIcon(IconNames.Surprised)
+        speed = 1
+        radio.sendValue("speed", 1)
+    }
+    logo_press += 1
+})
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     radio.sendValue("drive", 1)
 })
@@ -20,13 +28,25 @@ radio.onReceivedValue(function (name, value) {
     if (name == "drive") {
         if (value == 1) {
             bitbot.ledRainbow()
-            bitbot.rotate(BBRobotDirection.Left, 40)
+            if (speed == 0) {
+                bitbot.rotate(BBRobotDirection.Left, 20)
+            } else {
+                bitbot.rotate(BBRobotDirection.Left, 50)
+            }
         } else if (value == 2) {
             bitbot.ledRainbow()
-            bitbot.rotate(BBRobotDirection.Right, 40)
+            if (speed == 0) {
+                bitbot.rotate(BBRobotDirection.Right, 20)
+            } else {
+                bitbot.rotate(BBRobotDirection.Right, 50)
+            }
         } else if (value == 0) {
             bitbot.ledRainbow()
-            bitbot.go(BBDirection.Forward, 60)
+            if (speed == 0) {
+                bitbot.go(BBDirection.Forward, 30)
+            } else {
+                bitbot.go(BBDirection.Forward, 70)
+            }
         } else if (value == 8) {
             bitbot.ledRainbow()
             bitbot.go(BBDirection.Reverse, 60)
@@ -40,10 +60,21 @@ radio.onReceivedValue(function (name, value) {
             basic.showNumber(input.acceleration(Dimension.X))
             music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
         }
+    } else if (name == "speed") {
+        speed = value
+        if (value == 0) {
+            basic.showIcon(IconNames.Giraffe)
+        } else {
+            basic.showIcon(IconNames.Surprised)
+        }
     }
 })
+let logo_press = 0
+let speed = 0
 basic.showIcon(IconNames.Giraffe)
 radio.setGroup(1)
 bitbot.select_model(BBModel.XL)
 bitbot.ledClear()
 bitbot.BBBias(BBRobotDirection.Right, 10)
+speed = 0
+logo_press = 0
